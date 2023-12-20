c98b4400961571db7f95fbf2aa9bddd50b59dd32
savinoo <lulosavino@gmail.com>
2023-12-20 20:09:31 -0300

    feat: create 'visualizar-informacoes-pessoais' interface

--- domain/usecases/ivisualizar-informacoes-pessoas.ts
+++ domain/usecases/ivisualizar-informacoes-pessoas.ts
@@ -2,10 +2,8 @@
 import { Aluno } from './../model/aluno'
 
 export interface GetAluno {
-  get: (getIAluno: GetAluno.Params) => Promise<GetAluno.Result>
+  get: (matricula: string) => Promise<GetAluno.Result>
 }
 export namespace GetAluno {
-  export type Params = Omit<Aluno.Params, Exclude<keyof Aluno.Params, 'matricula'>>
-
   export type Result = Aluno.Params | false
 }
